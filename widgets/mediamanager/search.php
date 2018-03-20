5422a474ce41b8cd6cacce3b0a9b4d256675a2c9
SamPoyigi <sam@sampoyigi.com>
2018-03-20 17:41:25 +0000

    major refactoring

--- widgets/mediamanager/search.php
+++
@@ -1,6 +0,0 @@
-<input
-    type="text"
-    class="form-control"
-    value="<?= $searchTerm; ?>"
-    placeholder="<?= lang('text_filter_search'); ?>"
-    data-media-control="search"/>
--- widgets/mediamanager/statusbar.php
+++
@@ -1,9 +0,0 @@
-<nav class="navbar navbar-default navbar-statusbar" role="navigation">
-    <div class="container-fluid">
-        <p class="navbar-text">
-            <span data-media-total-size>0</span>
-            <span class="total-selected-text"> of </span>
-            <?= sprintf(lang('text_footer_note'), $totalItems, $folderSize); ?>
-        </p>
-    </div>
-</nav>
